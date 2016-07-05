3bdc617024622b27dc40674d9cdd9f3ab09c0bec
AlexandruPetreus <alexandru.petreus@softvision.ro>
2016-07-05 18:36:48 +0300

    second commit

--- DataAcces/GetCustomers.cs
+++ DataAcces/GetCustomers.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DataContracts;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,6 @@ using System.Threading.Tasks;
 
 namespace DataAcces
 {
-    public class Class1
-    {
-    }
+
 }
+
--- DataContracts/Models/AddressModel.cs
+++ DataContracts/Models/AddressModel.cs
@@ -9,7 +9,6 @@ namespace DataContracts.Models
     class AddressModel
     {
         public int AddressID { get; set; }
-        public string PhoneNumber { get; set; }
         public string Street { get; set; }
         public string City { get; set; }
         public string Country { get; set; }
